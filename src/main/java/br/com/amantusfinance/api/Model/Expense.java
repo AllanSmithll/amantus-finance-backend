a1419b3940a3b946db0d1dcbf9f29cf11ac343fb
allansmithll <allanammancio2004@gmail.com>
2023-12-20 13:22:13 -0300

    Novo mapeamento das classes

--- src/main/java/br/com/amantusfinance/api/Model/Expense.java
+++ src/main/java/br/com/amantusfinance/api/Model/Expense.java
@@ -3,26 +3,17 @@ package br.com.amantusfinance.api.Model;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
 import java.sql.Date;
 
+@EqualsAndHashCode(callSuper = true)
 @Entity
 @Table(name = "tb_expense")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class Expense {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    private String description;
-    private Double value;
-    private Date date;
-    private String category;
-    private String frequency;
+public class Expense extends Transaction {
     private String payment_method;
-    @Column(nullable = true)
-    private String add_information;
-
 }
--- src/main/java/br/com/amantusfinance/api/Model/Income.java
+++ src/main/java/br/com/amantusfinance/api/Model/Income.java
@@ -1,29 +1,20 @@
 package br.com.amantusfinance.api.Model;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
+        import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
 import java.sql.Date;
 
+@EqualsAndHashCode(callSuper = true)
 @Entity
 @Table(name = "tb_income")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class Income {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
-    private String description;
-    private Double value;
-    private Date date;
-    private String category;
-    private String frequency;
+public class Income extends Transaction {
     private String origin;
-    @Column(nullable = true)
-    private String add_information;
 
 }
